b59497fb5422a066d4b5cc52e2f894c57af424e9
chandrashekhar <csrm2204@gmail.com>
2023-06-06 20:19:38 +0530

    update
    complete

--- app.js
+++ app.js
@@ -33,3 +33,78 @@ mongoose.connect('mongodb+srv://app:app@cluster0.d7rchhc.mongodb.net/', {
       res.status(500).json({ error: 'Failed to retrieve items.' });
     }
   });
+  
+  app.get('/api/items/:id', async (req, res) => {
+    try {
+      const item = await Item.findById(req.params.id);
+      if (!item) {
+        return res.status(404).json({ error: 'Item not found.' });
+      }
+      res.json(item);
+    } catch (error) {
+      winston.error(`Failed to retrieve item: ${req.params.id}`, error);
+      res.status(500).json({ error: 'Failed to retrieve item.' });
+    }
+  });
+  
+  
+  app.post('/api/items', async (req, res) => {
+    try {
+      const item = new Item(req.body);
+      const savedItem = await item.save();
+      res.status(201).json(savedItem);
+    } catch (error) {
+      winston.error('Failed to create item:', error);
+      res.status(500).json({ error: 'Failed to create item.' });
+    }
+  });
+  
+
+  app.put('/api/items/:id', async (req, res) => {
+    try {
+      if (Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: 'No fields provided to update.' });
+      }
+  
+      const item = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      if (!item) {
+        return res.status(404).json({ error: 'Item not found.' });
+      }
+  
+      res.json(item);
+    } catch (error) {
+      winston.error('Failed to update item:', error);
+      res.status(500).json({ error: 'Failed to update item.' });
+    }
+  });
+  
+  
+app.delete('/api/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findByIdAndDelete(req.params.id);
+    if (!item) {
+      return res.status(404).json({ error: 'Item not found.' });
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    winston.error('Failed to delete item:', error);
+    res.status(500).json({ error: 'Failed to delete item.' });
+  }
+});
+
+
+  app.use((error, req, res, next) => {
+    winston.error('Unhandled error:', error);
+    res.status(500).json({ error: 'Something went wrong.' });
+  });
+
+winston.configure({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log' }),
+  ],
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
