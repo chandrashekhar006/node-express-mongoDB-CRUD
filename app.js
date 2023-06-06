a460c0367ff30b8342d2e332f79946944564baec
chandrashekhar <csrm2204@gmail.com>
2023-06-06 17:53:41 +0530

    express, mongo, schema
    -connected mongoDB
    -implemented json
    -implemented schema for item

--- app.js
+++ app.js
@@ -2,3 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const winston = require('winston');
 const port = 3000;
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://app:app@cluster0.d7rchhc.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    winston.info('Connected to the MongoDB database.');
+  })
+  .catch((error) => {
+    winston.error('Failed to connect to the MongoDB database:', error);
+  });
+
+
+  const itemSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+  });
+  
+  const Item = mongoose.model('Item', itemSchema);
